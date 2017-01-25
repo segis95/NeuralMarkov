e746d49db582d51ce9045dbd1180c0a25876a462
segis95 <segis95@mail.ru>
2017-01-25 19:48:17 +0100

    q

--- Neural/new/neural.py
+++ Neural/new/neural.py
@@ -1,91 +1,146 @@
+
 import numpy as np
 from pyDOE import *
 import matplotlib.pyplot as plt
 from sklearn import preprocessing
-
+from sklearn import tree
 
 def sigmoid(z):
-    return 1/(1.0 + np.exp(-z))
+    return 1.0/(1.0 + np.exp(-z))
 
 def L(x,y):
-    return -(x - y)**2
+    return (x - y)**2
     
-def forecast(w, x):
-    return sigmoid(w[0] * np.ones(10) + x * w[1:11]).mean()
+#sigmoid!!
+def forecast(w, x, c):
+    return activation((activation(x * w[:5], c)).dot(w[5:]), c)    
     
+def act1(z):
+    return (1.0 + np.tanh(z))/2.0
     
+def act2(z):
+    return ((2.0 * np.arctan(z)/np.pi) + 1.0)/2.0
     
-"""
-a = 2 * lhs(2, 400, criterion='maximin') - 1
-
-plt.plot(a[:,0], a[:,1],'o')
-plt.ylabel('some numbers')
-plt.show()
-"""
+def activation(z, c):    
+    if (c == 'arctan'):
+        return act2(z)
+    if (c == 'tanh'):
+        return act1(z)
+    return sigmoid(z)
 
 #Generating the train set and scaling it
 #For function  (1 + sin(x))/2 
 
-N = 100#number of points
+#N = number of points
+#M = number of generated expectations for each x
+#K = number of instances W for each expectation
 
-Train_x = np.linspace(-np.pi, np.pi, N)
-Train_y = ( 1.0 - np.sin(Train_x)) / 2.0
-
-scaler = preprocessing.StandardScaler().fit(Train_x)
-Train_x_scaled = scaler.transform(Train_x)
-
-M = 10#number of pairs for each x
-K = 10#number of instances W for each pair
-
-Pairs = {}
-Losses = {}
-
-sigma = np.random.rand()
-
-for i in range(len(Train_x_scaled)):
-    sigma = np.random.rand()
-    print( str(i) + "step")
-    v_set = 2.0 * lhs(11, M, criterion='maximin') - 1.0
-    error_to_pair = {}
-    #for each pair we generate matrices
+def learn_network(c, N = 100, M = 100, K = 10):
     
-    for j in range(M):#For every pair of parameters
-        SetW = [0 for q in range(K)]
-        
-        for k in range(K):#Generate matrix
-            Diag = [[sigma if r==s else 0 for r in range(11)] for s in range(11)]
-            SetW[k] = np.random.multivariate_normal((v_set[j,:]),Diag,1)
-            
-        
-        losses = [0 for q in range(K)]
-        
-        for q in range(K):#loss on matrix k
-            losses[q] =  L(sigmoid(SetW[q][0,0] * np.ones(10) + Train_x_scaled[i] * SetW[q][0,1:11]).mean(), Train_y[i])    
-            
-        Average_error = (np.array(losses)).mean()
-        error_to_pair[Average_error] = v_set[j,:]
+    #we generate the training set
+    Train_x = np.linspace(-np.pi, np.pi, N)
+    #function to approximate
+    Train_y = ( 1.0 + np.sin(Train_x)) / 2.0
     
-    min_error = max(error_to_pair.keys())
-    Pairs[Train_x_scaled[i]] = error_to_pair[min_error]
-    Losses[Train_x_scaled[i]] = min_error   
+    #we scale dataset
+    scaler = preprocessing.StandardScaler().fit(Train_x)
+    Train_x_scaled = scaler.transform(Train_x)
+    
+
+    #Dictionary of type {point:best_expectation_of_parameters}
+    Pairs = {}
+    
+    #Disctionary of type {}
+    Losses = {}
+    
+    #Variance parameter
+    sigma = 1.0
+    
+    #for each x we find best fitting vector of expectation
+    for i in range(len(Train_x_scaled)):
+        
+        print( str(i) + "step")
+        
+        #we generate a random set of parameters and scale them to [0,1]^10
+        v_set = 2.0 * lhs(10, M, criterion='maximin') - 1.0
+        
+        #maps average error to expectation_of_parameters(vector)
+        error_to_pair = {}
+         
+        for j in range(M):#For every vector of parameters(expectation)
+        
+            SetW = [0 for q in range(K)]
+            
+            for k in range(K):#We generate parameters
+                Diag = [[sigma if r==s else 0 for r in range(10)] for s in range(10)]
+                SetW[k] = np.random.multivariate_normal((v_set[j,:]),Diag,1)
+                
+            
+            losses = [0 for q in range(K)]
+            
+            for q in range(K):#we calculate loss on each instance of W
+                #sigmoid!!!   
+                losses[q] =  L(activation(activation( Train_x_scaled[i] * SetW[q][0,:5], c).dot(SetW[q][0,5:]), c), Train_y[i])    
+                
+            #we calculate average error for this instance of expectation vector
+            Average_error = (np.array(losses)).mean()
+            
+            #error -> parameter of exspectation realising it
+            error_to_pair[Average_error] = v_set[j,:]
+        
+        #we find the expectation that gives the minimum expected error and attach it the x 
+        min_error = min(error_to_pair.keys())
+        Pairs[Train_x_scaled[i]] = error_to_pair[min_error]
+        Losses[Train_x_scaled[i]] = min_error   
+        
+    
+    clf = tree.DecisionTreeRegressor(max_depth=8)
+    clf = clf.fit([[x] for x in Train_x_scaled], [Pairs[x] for x in Train_x_scaled])
+
+    
+    
+    test(scaler, Train_x_scaled, Train_y, clf, c)    
+    return (scaler, Train_x_scaled, Train_y, Pairs, clf)
         
 
+def test(scaler, Train_x_scaled, Train_y, clf, c):
+    
+    Test_x = np.linspace(-np.pi, np.pi, 23)
+    
+    Test_x_scaled = scaler.transform(Test_x)
+    
+    pd = clf.predict([[x] for x in Test_x_scaled])
+    
+    predictions = np.zeros(Test_x.shape[0])
+    
+    for i in range(Test_x.shape[0]):
+        predictions[i] = forecast(pd[i], Test_x_scaled[i], c)
+    
+    plt.plot(Test_x_scaled, predictions,'o', Train_x_scaled, Train_y)
+    plt.title("Activation function: " + c)
+    plt.savefig(c + "1.png")
 
 
-s = np.zeros(11)
 
-for i in Pairs.values():
-    s = s + i / len(Pairs.keys())
+#for i in Pairs.values():
+#    s = s + i / len(Pairs.keys())
   
+#np.array(Pairs.values()).mean(1)
 
-predictions = np.zeros(N)
-
+#forecast(Pairs[Train_x_scaled[i]]
+"""
+s = np.zeros(10)
 for i in range(N):
-    predictions[i] = forecast(s, Train_x_scaled[i])
+    predictions[i] = forecast(pd[i], Train_x_scaled[i])
     
 
 print("!!!")  
 plt.plot(Train_x_scaled, predictions,'o', Train_x_scaled, Train_y )
 plt.ylabel('some numbers')
 plt.show()
+
+"""
+
+
+
 
